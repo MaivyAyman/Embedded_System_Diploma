6a0a078c866e12e54fa94f84ce68d7795affbed4
Maivy Ayman <maivyayman@gmail.com>
2024-10-30 22:49:31 +0200

    Revert "C_Basics"
    This reverts commit 3c49785ace2e9d4c14a10d9de49d282031882e9b.

---
+++ C_Programming/C_Basics_HW1/EX1/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 30, 2024
+ *      Author: Maivy Ayman
+ */
+#include "stdio.h"
+
+void main (){
+
+	printf("Embedded System Diploma");
+
+}
+
---
+++ C_Programming/C_Basics_HW1/EX2/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 30, 2024
+ *      Author: Maivy Ayman
+ */
+
+#include "stdio.h"
+
+void main (){
+int num;
+	printf("Enter an integer :");
+	scanf("%d",&num);
+	printf("\nYou entered: %d ",num);
+
+}
+
