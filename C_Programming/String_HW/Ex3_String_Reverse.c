077522d2e5f4c9d371319aaf8cc07af13a69c361
Maivy Ayman <maivyayman@gmail.com>
2024-10-31 23:41:38 +0200

    add String_HW

---
+++ C_Programming/String_HW/Ex3_String_Reverse.c
@@ -0,0 +1,25 @@
+/*
+ *  Created on: Oct 31, 2024
+ *      Author: Maivy Ayman
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+	char str[100];
+	char rev[100]; 
+	int i,j=0;
+	
+    printf("Enter a string : \n");
+    gets(str);
+    
+	for(i=strlen(str)-1;i>=0;i--){
+	    rev[j]=str[i];
+	    j++;
+	}
+	printf("Reverse string is: %s \n",rev);
+ 
+	return 0;
+}
