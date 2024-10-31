12cfe5c33ff1ab4bdc3b5896025c2ba4265761bc
Maivy Ayman <maivyayman@gmail.com>
2024-10-31 23:39:35 +0200

    add Array_HW

---
+++ C_Programming/Array_HW/Ex2_AVG_By_Array.c
@@ -0,0 +1,27 @@
+/*
+ *  Created on: Oct 31, 2024
+ *      Author: Maivy Ayman
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	float a[10];
+	float avg=0;
+	int i,n;
+
+	printf("Enter the the number of data: \n");
+	scanf("%d", &n);
+	for (i=0;i<n;i++)
+	{
+		printf("Enter number: ");
+		scanf("%f", &a[i]);
+	}
+
+	for(i=0;i<n;i++){
+		avg += a[i];
+	}
+	printf("Average: %0.2f",avg/n);
+	return 0;
+}
